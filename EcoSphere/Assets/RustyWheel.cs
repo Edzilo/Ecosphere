854527e1aa850cf2132e330e99c09f15508cd946
Edzilo <Edzilo@yahoo.fr>
2020-07-09 15:27:38 +0200

    Added air pump sound

---
+++ EcoSphere/Assets/RustyWheel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RustyWheel : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+
+}
