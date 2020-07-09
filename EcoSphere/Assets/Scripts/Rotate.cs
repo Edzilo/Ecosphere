ad7eb884403b9c97154e941bc57bbfcf1172010f
Edzilo <Edzilo@yahoo.fr>
2020-07-09 14:53:19 +0200

    Cleaning up code

--- EcoSphere/Assets/Scripts/Rotate.cs
+++ EcoSphere/Assets/Scripts/Rotate.cs
@@ -4,25 +4,11 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-
-    private float x;
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        /*x += Time.deltaTime * rotationSpeed;
-        if (x > 360.0f)
-        {
-            x = 0.0f;
-        }*/
         transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotationSpeed);
-        //transform.localRotation = Quaternion.Euler(0, x, 0);
-
     }
 }
