19871fd14af52f28c8c3f0f55a370afbce98b360
Edzilo <Edzilo@yahoo.fr>
2020-07-09 14:51:59 +0200

    Air pump rotation

---
+++ EcoSphere/Assets/Scripts/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private float x;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*x += Time.deltaTime * rotationSpeed;
+        if (x > 360.0f)
+        {
+            x = 0.0f;
+        }*/
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotationSpeed);
+        //transform.localRotation = Quaternion.Euler(0, x, 0);
+
+    }
+}
