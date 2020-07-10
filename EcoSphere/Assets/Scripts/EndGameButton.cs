88193c142bb567908f0a82292f352b087146ca48
Edzilo <Edzilo@yahoo.fr>
2020-07-10 14:20:13 +0200

    Added main menu (basic)

--- EcoSphere/Assets/Scripts/EndGameButton.cs
+++ EcoSphere/Assets/Scripts/EndGameButton.cs
@@ -8,7 +8,7 @@ public class EndGameButton : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+        SceneManager.LoadSceneAsync("Niveau1"); // loads current scene
     }
 
     public void QuitGame()
