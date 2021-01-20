b15a2a41e2bc01b865bc904b0b33db93d385c9f1
Edzilo <Edzilo@yahoo.fr>
2021-01-20 15:39:19 +0100

    Update Menu + Credits

--- EcoSphere/Assets/Scripts/EndGameButton.cs
+++ EcoSphere/Assets/Scripts/EndGameButton.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class EndGameButton : MonoBehaviour
 {
+
+    public GameObject creditsPanel;
+
 
     public void RestartGame()
     {
@@ -24,6 +29,26 @@ public class EndGameButton : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadSceneAsync("Main Menu");
+    }
+
+    public void ShowCredits(bool isActive)
+    {
+        creditsPanel.SetActive(isActive);
+        EventSystem.current.SetSelectedGameObject(null);
+        Button button;
+
+        if (isActive)
+        {
+            button = creditsPanel.GetComponentInChildren<Button>();
+        }
+        else
+        {
+            button = GameObject.FindObjectOfType<Button>();
+        }
+        if (button != null)
+        {
+            EventSystem.current.SetSelectedGameObject(button.gameObject);
+        }
     }
 
 
